6e308fbd7ee6588447002d98b44141438b23727e
blugavere <b.lugavere@gmail.com>
2016-10-23 11:32:50 -0400

    current

--- src/tools/build.js
+++ src/tools/build.js
@@ -5,9 +5,9 @@ const webpack = require('webpack');
 const htmlBuilder = require('./htmlBuilder');
 
 const build = webpackConfig => {
-  
-  process.env.NODE_ENV = 'production';
-  console.log('Generating minified bundle for production via Webpack. This will take a moment.'.bold.green);
+  process.env.NODE_ENV = JSON.stringify('production');
+  //process.env.NODE_ENV = 'production';
+  console.log(`Generating minified bundle for ${process.env.NODE_ENV} via Webpack. This will take a moment.`.bold.green);
 
   webpack(webpackConfig).run((err, stats) => {
     if (err) {
@@ -16,7 +16,7 @@ const build = webpackConfig => {
     }
 
     const jsonStats = stats.toJson();
-    console.log(stats);
+
     if (jsonStats.hasErrors) {
       return jsonStats.errors.map(error => console.log(error.red));
     }
@@ -25,8 +25,11 @@ const build = webpackConfig => {
       console.log('Webpack generated the following warnings: '.bold.yellow);
       return jsonStats.warnings.map(warning => console.log(warning.yellow));
     }
+
     console.log('building html with hash: ', stats.hash);
+
     htmlBuilder.build(stats.hash);
+
     console.log(`Webpack stats: ${stats}`);
 
     console.log('Your app has been compiled in prodction mode and written to /dist. It\'s ready to roll!'.green);
--- webpack/modules.js
+++ webpack/modules.js
@@ -1,39 +1,48 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const fs = require('fs');
+const babelConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '../.babelrc')));
+babelConfig.presets = babelConfig.presets.map(x => `babel-preset-${x}`).map(require.resolve);
+babelConfig.plugins = babelConfig.plugins.map(x => `babel-plugin-${x}`).map(require.resolve);
 
 module.exports = {
   loaders: [{
-      test: /\.js$/,
-      include: [
-        path.join(__dirname, '../lib'),
-        path.join(__dirname, '../src')
-      ],
-      loaders: ['babel'],
-    }, {
-      test: /\.jsx$/,
-      include: [
-        path.join(__dirname, '../lib'),
-        path.join(__dirname, '../src')
-      ],
-      loaders: ['babel'],
-    }, {
-      test: /\.json$/,
-      loaders: ['json']
-    }, {
-      test: /\.ts$/,
-      loader: 'ts-loader'
-    }, {
-      test: /(\.css)$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap')
+    test: /\.js$/,
+    include: [
+      path.join(__dirname, '../lib'),
+      path.join(__dirname, '../src')
+    ],
+    loaders: ['babel'],
+    query: babelConfig
+  }, {
+    test: /\.jsx$/,
+    include: [
+      path.join(__dirname, '../lib'),
+      path.join(__dirname, '../src')
+    ],
+    loaders: ['babel'],
+    query: babelConfig
+  }, {
+    test: /\.json$/,
+    loaders: ['json']
+  }, {
+    test: /\.ts$/,
+    loader: 'ts-loader'
+  }, {
+    test: /(\.css)$/,
+    loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap')
       //loaders: ['style', 'css']
-    }, {
-      test: /\.less$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
-    }, {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
-    },
-    { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-    { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
-  ]
+  }, {
+    test: /\.less$/,
+    loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+  }, {
+    test: /\.scss$/,
+    loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+  }, {
+    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'url-loader?limit=10000&minetype=application/font-woff'
+  }, {
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'file-loader'
+  }]
 };
